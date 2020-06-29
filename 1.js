88b580ba6f213b4b092a446dbc97b6fdb94f6e6b
Nonthasut <nonthasut.phanphon@gmail.com>
2020-06-29 21:53:16 +0700

    foundBug3.js

--- 1.js
+++ 1.js
@@ -1,8 +1,8 @@
-let result = [2,3,5,7];
-for(i=2;i<=100;i++){
-    if(i%2!=0&&i%3!=0&&i%5!=0&&i%7!=0){
-    result.push(i)
-    }else{
-    continue;
+let result = [2, 3, 5, 7];
+for (i = 2; i <= 100; i++) {
+    if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+        result.push(i)
+    } else {
+        continue;
     }
-}alert(result)
+} alert(result)
