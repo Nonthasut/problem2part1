35f0f4d9fc305794b08cc58a6a3abab8609cbabe
Nonthasut <nonthasut.phanphon@gmail.com>
2020-07-04 22:01:43 +0700

    fixedForConditionRequire1.js

--- 1.js
+++ 1.js
@@ -1,16 +1,16 @@
 let result = []
-let check = []
-for (i = 2; i <= 500; i++) {
-    for (j = 0; j <= 100; j++) {
-        if (i % j == 0) {
-            check.push(i)
-        }
+let checker =0
+for (i = 2; i <= 100; i++) {
+    for(j=0;j<=100;j++){
+    if(i%j==0){
+        checker++
     }
-    if(check.length>2){
-    check=[]
-    continue
-    }else if(check.length===2){
-    result.push(i)
-    check=[]
     }
+    if(checker<3){
+        result.push(i)
+        checker=0
+    }else if(checker>2){
+        checker=0
+    }
+    
 } alert(result)
